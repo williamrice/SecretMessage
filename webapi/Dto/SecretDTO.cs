44548454ad3ff0bca90287108be900d94d60963b
William Rice <billyrice12@live.com>
2024-10-03 12:44:14 -0400

    created secret DTO

---
+++ webapi/Dto/SecretDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Dto
+{
+    public class SecretDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+
+    }
+}
