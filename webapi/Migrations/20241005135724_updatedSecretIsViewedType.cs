568261d13dbbae494e9cf388d4d26341395ee18a
William Rice <billyrice12@live.com>
2024-10-05 11:42:44 -0400

    db updates

---
+++ webapi/Migrations/20241005135724_updatedSecretIsViewedType.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedSecretIsViewedType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.DropColumn(name: "IsViewed", table: "Secrets");
+            migrationBuilder.AddColumn<bool>(
+                name: "IsViewed",
+                table: "Secrets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "IsViewed", table: "Secrets");
+            migrationBuilder.AddColumn<bool>(
+                name: "IsViewed",
+                table: "Secrets",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false
+            );
+        }
+    }
+}
